c3d3626279caaa5bddef5dc18719ab07696bd4b9
Ali <Alihunter4640@gmail.com>
2024-05-20 00:19:41 +0330

    Video model and admin
    made

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import video
 # Register your models here.
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date','views')
+    list_filter = ('title',)
+    empty_value_display = "-empty-"
+    search_fields = ('title','description')
+    save_as = True 
+    save_on_top = True 
+    search_help_text = f'search in: {". ".join(search_fields)}'
+
+
+
+
+admin.site.register(video,VideoAdmin)
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from taggit.managers import TaggableManager
+
 
 # Create your models here.
+class video(models.Model):
+    title = models.CharField(max_length=255)
+    published_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(null=True,blank=True)
+    video = models.FileField(upload_to='videos/')
+    tags = TaggableManager()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+    
+    class Meta:
+        ordering = ['-published_date']
